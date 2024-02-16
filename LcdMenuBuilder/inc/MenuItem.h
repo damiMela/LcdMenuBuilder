374d966536dadfca08ed59b61ce5d7d8b0c03cb6
damimela-frba <146790144+damimela-frba@users.noreply.github.com>
2024-02-16 10:24:23 -0300

    Refactor: MenuItem.
    Refactored by vsCode

--- LcdMenuBuilder/inc/MenuItem.h
+++ LcdMenuBuilder/inc/MenuItem.h
@@ -11,9 +11,12 @@
 #include "Menu_common.h"
 #include "MenuConfig.h"
 #include <functional>
+#include <memory>
 
-namespace MenuNav{
-	typedef enum{
+namespace MenuNav
+{
+	typedef enum
+	{
 		ARROW_UP,
 		ARROW_DOWN,
 		ARROW_LEFT,
@@ -21,45 +24,52 @@ namespace MenuNav{
 		ENTER_KEY,
 		CANCEL_KEY,
 		BACKSPACE_KEY
-	}MenuNavInput_e;
+	} MenuNavInput_e;
 }
 
 class MenuItem;
 typedef uint8_t menuId;
-typedef std::list<MenuItem*> NavStack;
+typedef std::list<std::shared_ptr<MenuItem>> NavStack;
 
-class MenuItem {
+class MenuItem
+{
 public:
 	MenuItem(const std::string title);
 
 	virtual ~MenuItem();
-	virtual void Execute() {	callback(this);	}
-	virtual void Cancel(){ navStack.pop_back();}
+	virtual void Execute() { callback(this); }
+	virtual void Cancel() { navStack.pop_back(); }
 	virtual void Input(char input){};
-	virtual void Render(){
-		for (int i = 0; i < MenuConfig::heigth; i++) MenuConfig::print(i, "");
+	virtual void Render()
+	{
+		for (int i = 0; i < MenuConfig::heigth; i++)
+			MenuConfig::print(i, "");
 	}
-	virtual void Input(MenuNav::MenuNavInput_e input){
-		if(input==MenuNav::CANCEL_KEY) navStack.back()->Cancel();
+	virtual void Input(MenuNav::MenuNavInput_e input)
+	{
+		if (input == MenuNav::CANCEL_KEY)
+			navStack.back()->Cancel();
 	}
 
-	menuId GetId(){return id;}
-	std::string GetTitle() { return title;	}
-	static uint8_t NavStackSize() {return navStack.size();}
-	void SetCallback(std::function<void(MenuItem*)> callback){ this->callback=callback;};
+	menuId GetId() { return id; }
+	std::string GetTitle() { return title; }
+	static uint8_t NavStackSize() { return navStack.size(); }
+	void SetCallback(std::function<void(MenuItem *)> callback) { this->callback = callback; };
 
 protected:
 	std::string title;
 	menuId id;
-	std::function<void(MenuItem*)>  callback;
-	void* args = nullptr;
+	std::function<void(MenuItem *)> callback;
+	void *args = nullptr;
 	size_t argsLen = 0;
 
 	static NavStack navStack;
 	static uint8_t globalItemCounter;
-	static void NullCallback(){ std::cout << "default" << std::endl; sleep(1);}
-
+	static void NullCallback()
+	{
+		std::cout << "default" << std::endl;
+		sleep(1);
+	}
 };
-
 
 #endif /* MENUITEM_H_ */
